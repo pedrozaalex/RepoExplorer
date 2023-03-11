d19d908cb1c324705a8944b258c74ce68aa2162d
Alexandre Pedroza <pedrozaalexandre@gmail.com>
2023-03-11 09:54:15 -0300

    💚 Add polyfills

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import type { UserConfig } from 'vite';
 import wasmPack from 'vite-plugin-wasm-pack';
+import polyfillNode from 'vite-plugin-node-stdlib-browser';
 
 const config: UserConfig = {
-	plugins: [sveltekit(), wasmPack([], ['pyranine'])],
+	plugins: [sveltekit(), wasmPack([], ['pyranine']), polyfillNode()],
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
 	},
